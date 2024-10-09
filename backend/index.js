525cb68157497968a5f881283d414e8492a7d950
KheteshS <kheteshs@gmail.com>
2024-10-09 23:20:13 +0530

    project init

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const rootRouter = require("./routes/index");
+
+const cors = require("cors");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use("/api/v1", rootRouter);
+
+app.listen(PORT, (err) => {
+  if (err) console.error(err);
+  console.log(`Server running on port ${PORT}`);
+});
